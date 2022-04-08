b4fcf44cafeafb7152b86d5bd8bcae278fbdfd5e
Morgan.Arrington <Morgan.Arrington@nmfs.local>
2022-04-08 12:30:39 -0700

    clean up

--- Part 3 - bootstrap resample ages/model_fitting.R
+++ Part 3 - bootstrap resample ages/model_fitting.R
@@ -5,22 +5,34 @@
 library(dplyr)
 library(purrr)
 library(mdatools)
+library(furrr)
+library(future)
 
 # library(plotly)
 # library(stringr)
 # library(ggpubr)
 
+# load data
+load("Z:/NIR-ageing-simulation/Data/boot_dat_10_og.rda")
+load("Z:/NIR-ageing-simulation/Data/all_dat_filt.rda")
+
 # write function for passing PLS model to each dataset with jittered age
 # test
 # Might need to store jittered age preds in a matrix, each column different iter. Then fit models in a loop on each subsequent row of response data. Extract only the predictions.
-
 jitter_age_mod <- function(samp) {
-  mod <- pls(all_dat[, c(6:505)], samp, scale = TRUE, ncomp = 10, cv = 50)
+  mod <- pls(all_dat[, c(7:506)], samp, scale = TRUE, ncomp = 10, cv = 50)
   x <- mod$cvres$ncomp.selected #allow each to use optimal ncomp
   mod$cvres$y.pred[,x,1] #use x to select preds from matrix
 }
 
 jitter_mod_list <- map(samp, jitter_age_mod)
+
+n_cores <- availableCores()-2
+plan(multisession, workers = n_cores)
+
+system.time(
+jitter_mod_list <- future_map(samp, jitter_age_mod, seed=TRUE)
+)
 
 # # Pull out predictions and make a matrix
 # extract_preds <- function(mod) {
@@ -31,7 +43,7 @@ jitter_mod_list <- map(samp, jitter_age_mod)
 # jitter_mod_preds <- map(jitter_mod_list, extract_preds) #apply function to mod list
 
 # Make a dataframe that has known_age, error_age, known_age_preds, error_age_preds from each Iter
-
+Iter <- 10
 jitter_mod_preds <- data.frame(matrix(unlist(jitter_mod_list), ncol=Iter, byrow=F)) 
 
 jitter_age <- data.frame(matrix(unlist(samp), ncol = Iter, byrow = F))
@@ -51,4 +63,4 @@ names <- c("ID", colnames(age_df), names1, names2)
 colnames(preds_df) <- paste(names)
 
 # Output data
-write.csv(preds_df, "C:/Users/marri/OneDrive/Documents/AFSC A&G Contract/Simulation Project/Data/part 3 preds_indageerror_iter10_2013.csv") # preds_df - this can be used to calculate the rest
+write.csv(preds_df, "Z:/NIR-ageing-simulation/Data/part 3 preds_indageerror_iter10_2013_og.csv") # preds_df - this can be used to calculate the rest
